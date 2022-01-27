cb84c3f1819e258cb0b0eb5a25ef3dba0fa1b154
mohamadRasul mirzabeygi <its.rasulmirzabeygi@gmail.com>
2022-01-27 11:12:56 +0330

    implement basic filter getALlTours controller

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -2,10 +2,22 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const exclude = ['sort', 'limit', 'page', 'fields'];
+    exclude.forEach((el) => delete queryObj[el]);
 
+    const query = Tour.find(queryObj);
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(req.query.duration)
+    //   .where('difficulty')
+    //   .equals(req.query.difficulty);
+
+    const tours = await query();
     res.status(200).json({
       status: 'success',
+      result: tours.length,
       data: {
         tours,
       },
---
+++ dev-data/data/import-and-delete-dev-data.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const Tour = require('../../models/tourModel');
+require('dotenv').config();
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('mongoose is running well ...'));
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+// DELETE DATA TOURS FROM DB
+// eslint-disable-next-line no-unused-vars
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('All data deleted in tours collection successfully!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// IMPORT DATA TOURS FROM DB
+// eslint-disable-next-line no-unused-vars
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// ******** delete func will delete All the docs in tours collection ************
+// deleteData();
+// ******** import func will import All the tours-simle.json to tours collection ************
+// importData();
